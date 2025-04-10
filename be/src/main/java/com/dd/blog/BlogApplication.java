b987884f13a7e2e7bec457d76149cdc94f512204
Jaehyun Yoo <jaehyun.yoo@stonybrook.edu>
2025-04-10 11:51:18 +0900

    test

--- be/src/main/java/com/dd/blog/BlogApplication.java
+++ be/src/main/java/com/dd/blog/BlogApplication.java
@@ -3,7 +3,7 @@ package com.dd.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//check
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
